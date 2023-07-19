32357016b8a59e45b6f037f4f4ab1138517f34ee
Oleg Markov <o.mark-mail.ru@yandex.ru>
2023-07-19 22:44:19 +0200

    поправлен app.js

--- backend/app.js
+++ backend/app.js
@@ -83,6 +83,6 @@ app.use(errorHandler);
 //   sameSite: 'none',
 // }));
 
-app.listen(3001, () => {
-  console.log('App listening on port 3001');
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
 });
