1bb23f504b7d24c3ae0b2b9de2c126d30504f6fb
Oleg Markov <o.mark-mail.ru@yandex.ru>
2023-07-22 17:12:09 +0200

    исправлен app.js

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-// const rateLimit = require('express-rate-limit');
-// const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const app = express();
-const cors = require('./middlewares/cors');
-// const cors = require('cors');
+// const cors = require('./middlewares/cors');
+const cors = require('cors');
 const userRoute = require('./routes/users');
 const cardRoute = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
@@ -20,10 +21,12 @@ const {
 const errorHandler = require('./middlewares/errors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   max: 5000,
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5000,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
@@ -31,35 +34,41 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cookieParser());
-app.use('*', cors());
-// app.use('*', cors({
-//   origin: [
-//     'https://api.markov.project.nomoredomains.work',
-//     'http://api.markov.project.nomoredomains.work',
-//     'https://markov.project.nomoreparties.sbs',
-//     'http://markov.project.nomoreparties.sbs',
-//     'https://localhost:3000',
-//     'http://localhost:3000',
-//     'https://localhost:3001',
-//     'http://localhost:3001',
-//     'localhost:3000',
-//   ],
-// }));
+app.use(cookieParser({
+  secret: 'oleg-secrets',
+  secure: true,
+  httpOnly: true,
+  sameSite: 'none',
+}));
+// app.use('*', cors());
+app.use('*', cors({
+  credentials: true,
+  origin: [
+    'https://api.markov.project.nomoredomains.work',
+    'http://api.markov.project.nomoredomains.work',
+    'https://markov.project.nomoreparties.sbs',
+    'http://markov.project.nomoreparties.sbs',
+    'https://localhost:3000',
+    'http://localhost:3000',
+    'https://localhost:3001',
+    'http://localhost:3001',
+    'localhost:3000',
+  ],
+}));
 
-// app.enable('trust proxy');
-// app.use(rateLimit);
-// app.use(helmet());
-// app.use(limiter);
+app.enable('trust proxy');
+app.use(rateLimit);
+app.use(helmet());
+app.use(limiter);
 
-// app.use(express.json());
+app.use(express.json());
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
 app.post('/signin', validateLogin, login);
 app.post('/signup', validateUser, createUser);
